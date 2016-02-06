d7d3f0bb315382ca746e0bc4f21c5300f5b7df80
Mark P. Lindhout <mark@twb.berlin>
2016-02-06 15:59:10 +0100

    Added Chrome options page

--- template/chrome/options.js
+++ template/chrome/options.js
@@ -10,40 +10,59 @@ com.marklindhout.wdbms = com.marklindhout.wdbms || {};
 // Extension options
 com.marklindhout.wdbms.options = {
 
-	defaults: {
-		url: false,
-		username: false,
-		password: false
+	allowed_options: [
+		'url',
+		'username',
+		'password'
+	],
+
+	show_status_message: function (message) {
+		var $s = $('#status');
+		$s.hide();
+		$s.text( message );
+		$s.fadeIn()
+		  .delay(750)
+		  .fadeOut();
+	},
+
+	is_allowed_option: function (option_name) {
+		var allowed_options = com.marklindhout.wdbms.options.allowed_options;
+		return allowed_options.indexOf(option_name) > -1;
 	},
 
 	save_to_storage: function () {
 
-		var options_object = {
-			url: $('#url').value(),
-			username: $('#username').value(),
-			password: $('#password').value()
-		};
+		var options_object = {};
+		var allowed_options = com.marklindhout.wdbms.options.allowed_options;
 
-		chrome.storage.sync.set( options_object, function () {
-			// Update status to let user know options were saved.
-			var status         = document.getElementById('status');
-			status.textContent = 'Options saved.';
-			setTimeout(function () {
-				status.textContent = '';
-			}, 750);
+		for ( index in allowed_options ) {
+
+			var option = allowed_options[index];
+
+			options_object[ option ] = $('#' + option).val() || false;
+		}
+
+		chrome.storage.sync.set(options_object, function () {
+			com.marklindhout.wdbms.options.show_status_message('Options saved.')
 		});
 
 	},
 
 	restore_from_storage: function () {
 
-		// Use default value color = 'red' and likesColor = true.
-		chrome.storage.sync.get({
-			favoriteColor: 'red',
-			likesColor   : true
-		}, function ( items ) {
-			document.getElementById('color').value  = items.favoriteColor;
-			document.getElementById('like').checked = items.likesColor;
+		chrome.storage.sync.get(null, function ( obj ) {
+
+			var allowed_options = com.marklindhout.wdbms.options.allowed_options;
+
+			for ( index in allowed_options ) {
+
+				var option = allowed_options[index];
+				var $item = $('#' + option);
+
+				if ( $item.length > 0 ) { // See if this field exists
+					$item.val( obj[option] );
+				}
+			}
 		});
 	},
 
@@ -56,7 +75,7 @@ com.marklindhout.wdbms.options = {
 $(document).ready(function () {
 	com.marklindhout.wdbms.options.init();
 
-	$('#save').on('click', function (event) {
+	$('#save').on('click', function ( event ) {
 
 		event.preventDefault();
 
